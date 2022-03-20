ccb44758625f1ba25d6ad34f7f4fd0fc88b440f9
EC2 Default User <ec2-user@ip-172-31-32-227.ec2.internal>
2022-03-20 09:10:02 +0000

    [add]会員詳細画面作成途中,カラム不足

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,6 +2,9 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.page(params[:page])
     @customer = Customer.new
+  end
 
+  def show
+    @customer = Customer.find(params[:id])
   end
 end
