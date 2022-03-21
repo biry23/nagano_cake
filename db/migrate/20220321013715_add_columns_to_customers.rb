01fa31bb86f3ae7debdce7bcd0dc645ffd5eca00
EC2 Default User <ec2-user@ip-172-31-32-227.ec2.internal>
2022-03-21 01:46:13 +0000

    [Fix]customersのカラム追加

---
+++ db/migrate/20220321013715_add_columns_to_customers.rb
@@ -0,0 +1,10 @@
+class AddColumnsToCustomers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :customers, :last_name, :string
+    add_column :customers, :first_name, :string
+    add_column :customers, :last_name_kana, :string
+    add_column :customers, :first_name_kana, :string
+    add_column :customers, :postal_code, :string
+    add_column :customers, :address, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_091601) do
+ActiveRecord::Schema.define(version: 2022_03_21_013715) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 2022_03_17_091601) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "is_deleted", default: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.string "address"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
