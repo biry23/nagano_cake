a1f7e6fe379aaaeb9082039793540e05ee038061
EC2 Default User <ec2-user@ip-172-31-32-227.ec2.internal>
2022-03-22 09:21:15 +0000

    [add]注文履歴詳細画面機能作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,11 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_detail = @order.order_details
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,3 +1,5 @@
 class OrderDetail < ApplicationRecord
   has_many :items
+  enum maiking_status: { not_produced: 0, waiting_for_deposit: 1, production: 2, complete: 3 }
+
 end
