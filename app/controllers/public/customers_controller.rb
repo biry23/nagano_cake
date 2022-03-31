ca86e37663b1ea8298b821610e1822becc42f31f
biry23 <ito.r.okr@gmail.com>
2022-03-31 10:28:49 +0000

    [add]public会員情報編集画面機能作成,退会機能途中

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,4 +3,24 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
   end
 
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path
+    else
+      render :show
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,29 @@
 Rails.application.routes.draw do
 
-  devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/customers/registrations",
-  sessions: 'public/customers/sessions'
-}
+
 
   # publicのアクション
   scope module: 'public' do
+    
     root to: "homes#top"
     get "/about" => "homes#about", as: "about"
+    get "/customers/my_page" => "customers#show"
+    get "/customers/edit" => "customers#edit"
+    patch "/customers" => "customers#update"
+    get "/customers/confirm" => "customers#confirm"
+    patch "/customers/withdraw" => "customers#withdraw"
     resources :homes, only: [:top, :about]
     resources :items, only: [:show, :index]
-    resources :registrations, only: [:new, :create]
+    resources :registrations, only: [:new, :create],except:[:cancel, :edit, :update, :destroy]
     resources :sessions, only: [:new, :create, :destroy]
-    resources :customers, only: [:show, :edit, :update, :confirm, :withdraw]
     resources :cart_items, only: [:create, :index, :update, :destroy, :destroy_all]
     resources :orders, only: [:new, :index, :show, :confirm, :complete, :create]
     resources :addresses, only: [:edit, :index, :create, :update, :destroy]
   end
-
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/customers/registrations",
+    sessions: 'public/customers/sessions'
+  }
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
