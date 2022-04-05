cb71f7d2d1567c71a95f45c5a4f225602e2f4da0
biry23 <ito.r.okr@gmail.com>
2022-04-05 10:43:25 +0000

    [add]注文情報入力画面機能作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,32 @@
 class Public::OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to orders_complete_path
+    else
+      render :confirm
+    end
+  end
+
+  def confirm
+  end
+
+  def complete
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
+
 end
