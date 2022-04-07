65e994d76ac3d34e1c1bdd2fcc7506cc427b2317
biry23 <ito.r.okr@gmail.com>
2022-04-07 11:59:59 +0000

    [Fix]注文確認画面confirm/create作成,注文完了後のカート内を空にする機能作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,7 +12,9 @@ class Public::OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
     if @order.save
+      current_customer.cart_items.destroy_all
       redirect_to orders_complete_path
     else
       render :confirm
@@ -25,11 +27,11 @@ class Public::OrdersController < ApplicationController
     @total_price = 0
     @order.shipping_cost = 800
 
-    if params[:order][:my_address] == 0
+    if params[:order][:my_address] == "0"
       @order.postal_code = current_customer.postal_code
       @order.name = current_customer.last_name + current_customer.first_name
       @order.address = current_customer.address
-    elsif params[:order][:my_address] == 1
+    elsif params[:order][:my_address] == "1"
       @address = Address.find(params[:order][:address_id])
       @order.postal_code = @address.postal_code
       @order.name = @address.name
