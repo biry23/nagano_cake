f80748e864b99787f0cab0338636bc440a869c08
biry23 <ito.r.okr@gmail.com>
2022-04-11 13:07:48 +0000

    [Fix]admin注文履歴詳細作成途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,12 +2,16 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order.status = 0
-    @order_detail = @order.order_details
-    @order_detail.maiking_status = 0
+    @order_details = @order.order_detail
+    
   end
 
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
   end
+  
+  private
+  def order_detail_params
+    params.require(:order).permit(:)
 end
