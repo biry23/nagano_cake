ec9418edbdc82e652f162767e50c4c894fb93834
biry23 <ito.r.okr@gmail.com>
2022-04-12 07:12:14 +0000

    [Fix]カート内商品個数変更機能追加,updateアクション追加,更新ボタン追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
     @customer = Customer.page(params[:page])
     @customers = Customer.all
@@ -11,7 +13,7 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
@@ -20,10 +22,10 @@ class Admin::CustomersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
   end
-  
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,17 +1,14 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
   def show
     @order = Order.find(params[:id])
     @order.status = 0
     @order_details = @order.order_detail
-    
+
   end
 
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
   end
-  
-  private
-  def order_detail_params
-    params.require(:order).permit(:)
 end
