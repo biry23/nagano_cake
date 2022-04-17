c02ac2a1445be735c97c62c2c629ce3a0a7a87c6
biry23 <ito.r.okr@gmail.com>
2022-04-17 07:38:59 +0000

    [Fix]注文ステータスと製作ステータスの連動した実装作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -13,8 +13,10 @@ class Admin::OrdersController < ApplicationController
     # 注文ステータスのupdate
     @order = Order.find(params[:id])
     @order.update(order_params)
-    if params[:order][:status] == "入金確認"
-      order_details.update(order_detail_params)
+    # 製作ステータスの自動更新
+    if params[:order][:status] == "payment_confirmation"
+      @order.order_details.update_all(making_status: 1)
+      # update_allにすることで注文商品の製作ステータスが更新される
     end
     redirect_to admin_order_path(@order)
   end
@@ -22,8 +24,21 @@ class Admin::OrdersController < ApplicationController
   def making_status_update
     # 製作ステータスのupdate
     @order_detail = OrderDetail.find(params[:id])
-    @order_detail.update(order_detail_params)
-    redirect_to admin_order_path(@order_detail.order_id)
+    @order = @order_detail.order
+    @one_order = @order.order_details
+
+    if @order_detail.update(order_detail_params)
+    # 注文ステータスの自動更新
+      if params[:order_detail][:making_status] == "production"
+        @order_detail.order.update(status: 2)
+      end
+
+      if @one_order.count == @one_order.where(making_status: "complete").count
+        # １件に紐づいたorderのorder_detailsの数 == 左辺のorder_detailsのmaking_statusがcompleteになっている数
+        @order_detail.order.update(status: 3)
+      end
+      redirect_to admin_order_path(@order_detail.order_id)
+    end
   end
 
   private
